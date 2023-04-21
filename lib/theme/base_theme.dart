ba0627b01284d5fb1970f78d0829d253e245b138
celeste0423 <jyeob25@korea.ac.kr>
2023-04-21 10:12:48 +0900

    initial commit 1

---
+++ lib/theme/base_theme.dart
@@ -0,0 +1,34 @@
+import 'package:couple_to_do_list_app/utils/custom_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+ThemeData baseTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    scaffoldBackgroundColor: CustomColors.backgroundLightGrey,
+    appBarTheme: AppBarTheme(
+      backgroundColor: CustomColors.mainPink,
+      elevation: 0,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.dark,
+      ),
+      iconTheme: IconThemeData(
+        color: Colors.black.withOpacity(0.5),
+      ),
+    ),
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: CustomColors.backgroundLightGrey,
+      onPrimary: CustomColors.backgroundLightGrey,
+      secondary: CustomColors.backgroundLightGrey,
+      onSecondary: CustomColors.backgroundLightGrey,
+      error: CustomColors.backgroundLightGrey,
+      onError: CustomColors.backgroundLightGrey,
+      background: CustomColors.backgroundLightGrey,
+      onBackground: CustomColors.backgroundLightGrey,
+      surface: CustomColors.backgroundLightGrey,
+      onSurface: CustomColors.backgroundLightGrey,
+    ),
+  );
+}
---
+++ lib/utils/custom_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+  CustomColors._();
+
+  static const Color mainPink = Color(0xFFFAA7A7);
+  static const Color lightPink = Color(0xFFFCD1D1);
+
+  static const Color backgroundLightGrey = Color(0xFFEFEFEF);
+  static const Color grey = Color(0xFF7A7A7A);
+  static const Color darkGrey = Color(0xFF707070);
+}
