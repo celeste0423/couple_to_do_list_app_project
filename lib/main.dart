58c3cc2bdead02ea31cdee7c69e20f2105c6bcf5
celeste0423 <jyeob25@korea.ac.kr>
2023-04-26 16:03:03 +0900

    appIcon / nativeSplashScreen added, firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
-
 import 'package:couple_to_do_list_app/features/home/pages/home_page.dart';
+import 'package:couple_to_do_list_app/firebase_options.dart';
 import 'package:couple_to_do_list_app/theme/base_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle.dark.copyWith(
@@ -20,12 +29,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove(); //앱 로딩 후 제거해주는 걸로 수정할 것
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bukkung List',
       theme: baseTheme(),
       home: HomePage(),
-      // home: WelcomePage(),
+      // home: GetX<UserInfoAuthController>(
+      //   init: UserInfoAuthController(),
+      //   builer: (controller){
+      //     FlutterNativeSplash.remove();
+      //     return const WelcomPage();
+      //   } else {
+      //     return const HomePage();
+      //  }
+      // )
+
+      // onGenerateRoute: Routes.onGenerateRoute,
     );
   }
 }
