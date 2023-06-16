bf6d4fad76fb3189191db966918f7a8a2a188648
celeste0423 <jyeob25@korea.ac.kr>
2023-06-16 10:02:58 +0900

    디테일 페이지 UI 완성

--- lib/features/bukkung_list_detail/pages/bukkung_list_detail_page.dart
+++ lib/features/bukkung_list_detail/pages/bukkung_list_detail_page.dart
@@ -31,6 +31,36 @@ class BukkungListDetailPage extends StatelessWidget {
       title: TitleText(
         text: '상세보기',
       ),
+      actions: [
+        PopupMenuButton(
+          offset: Offset(0, 50),
+          shape: ShapeBorder.lerp(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+            1,
+          ),
+          itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(
+                onTap: () {},
+                child: Text(
+                  "수정하기",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Yoonwoo',
+                    letterSpacing: 1.5,
+                    color: CustomColors.darkGrey,
+                  ),
+                ),
+              ),
+            ];
+          },
+          child: Icon(
+            Icons.more_vert,
+            size: 35,
+          ),
+        ),
+      ],
     );
   }
 
@@ -55,15 +85,26 @@ class BukkungListDetailPage extends StatelessWidget {
               ],
             ),
           ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              bukkungListModel.title!,
-              style: TextStyle(
-                color: CustomColors.blackText,
-                fontSize: 35,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                bukkungListModel.title!,
+                style: TextStyle(
+                  color: CustomColors.blackText,
+                  fontSize: 35,
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20, top: 10),
+                child: Text(
+                  bukkungListModel.date!.toString().substring(0, 10),
+                  style: TextStyle(
+                    color: CustomColors.greyText,
+                  ),
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
@@ -84,9 +125,27 @@ class BukkungListDetailPage extends StatelessWidget {
               Row(
                 children: [
                   PngIcon(iconName: 'location-pin'),
+                  Text(
+                    bukkungListModel.location!,
+                    style: TextStyle(
+                      color: CustomColors.blackText,
+                    ),
+                  ),
                 ],
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                bukkungListModel.content!,
+                style: TextStyle(
+                  color: CustomColors.blackText,
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -94,13 +153,28 @@ class BukkungListDetailPage extends StatelessWidget {
   }
 
   Widget _bottomBar() {
-    return Row(
-      children: [
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.edit_outlined),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          width: 140,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: CustomColors.mainPink,
+          ),
+          child: Center(
+            child: Text(
+              '리스트 완료',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+          ),
         ),
-      ],
+      ),
     );
   }
 
