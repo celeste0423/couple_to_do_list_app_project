ea42574dc16a4a0c5efd07426b31631e14f078cc
DentKim <ddosy99@gmail.com>
2023-07-31 19:52:37 +0900

    관리자페이지 사진 url업로드하기

--- lib/features/admin_management/pages/admin_management.dart
+++ lib/features/admin_management/pages/admin_management.dart
@@ -1,11 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:couple_to_do_list_app/features/admin_management/pages/admin_list_suggestion_page.dart';
+import 'package:couple_to_do_list_app/helper/open_alert_dialog.dart';
 import 'package:couple_to_do_list_app/repository/user_repository.dart';
 import 'package:couple_to_do_list_app/widgets/main_button.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AdminPage extends StatelessWidget {
   const AdminPage({Key? key}) : super(key: key);
+
+  upLoadDefaultBukkungList() async {
+    CollectionReference fireStorecollection =
+    FirebaseFirestore.instance.collection('bukkungLists');
+    final storageRef = FirebaseStorage.instance.ref();
+
+    for (int i = 1; i <= 150; i++) {
+      String paddedIndex = i.toString().padLeft(3, '0');
+      String docName = 'defaultBukkungList' + i.toString().padLeft(3, '0');
+
+      String imageUrl; // Declare imageUrl outside the first try block
+
+      try {
+        imageUrl = await storageRef
+            .child("suggestion_bukkunglist/$docName.jpg")
+            .getDownloadURL();
+      } catch (e) {
+        // Handle the exception here (e.g., print a specific error message)
+        print("Error fetching download URL for $paddedIndex: $e");
+        break;
+        // Alternatively, if you want to stop the loop immediately after the first exception,
+        // you can use 'return;' instead of 'break;'.
+      }
+
+      // Move the second try block inside the first try block's scope
+      try {
+        await fireStorecollection.doc(docName).set({'imgUrl': imageUrl});
+        print("URL for $docName uploaded successfully.");
+      } catch (e) {
+        print("Error updating Firestore document for $docName: $e");
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +55,19 @@ class AdminPage extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             children: [
+              MainButton(
+                buttonText: '기본버꿍리스트 업로드',
+                buttonColor: Colors.grey,
+                onTap: () async {
+                  await upLoadDefaultBukkungList();
+                  openAlertDialog(title: '업로드 완료');
+                },
+              ),
+              MainButton(
+                buttonText: '사진 URL: Storage->FireStore',
+                buttonColor: Colors.grey,
+                onTap: () {},
+              ),
               MainButton(
                 buttonText: '추천 버꿍리스트 페이지',
                 buttonColor: Colors.grey,
