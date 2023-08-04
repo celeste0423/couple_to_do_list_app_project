b98fb1940e9ebf360162e53b7b32dd76d47b1b57
DentKim <ddosy99@gmail.com>
2023-08-05 03:08:45 +0900

    회원탈퇴 수정

--- lib/features/setting/controller/auth_delete_page_controller.dart
+++ lib/features/setting/controller/auth_delete_page_controller.dart
@@ -16,15 +16,20 @@ class AuthDeletePageController extends GetxController {
 
   void authDeletion() async {
 
-    //uid 임시저장
-    String? uid = AuthController.to.user.value.uid;
-    String? groupId = AuthController.to.user.value.groupId;
-    String? myGender = AuthController.to.user.value.gender;
+
+
     //auth 삭제 전 재인증
     await openAlertDialog(
         title: '삭제',
         content: '원활한 삭제를 위해 계정 인증 부탁드립니다.',
+        secondButtonText:'뒤로가기',
+        btnText: '인증 후 탈퇴하기',
         mainfunction: () async {
+          //uid 임시저장
+          String? uid = AuthController.to.user.value.uid;
+          String? groupId = AuthController.to.user.value.groupId;
+          String? myGender = AuthController.to.user.value.gender;
+
           String loginType = await UserRepository.getLoginType();
           if (loginType == 'google') {
             final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -62,37 +67,37 @@ class AuthDeletePageController extends GetxController {
               // AuthController.loginType = 'apple';
             }
           }
-        });
-    //auth에서 삭제
-    await FirebaseAuth.instance.currentUser!.delete();
-    print('auth 삭제뒤');
-    //유저 파이어스토어 삭제
-    await FirebaseFirestore.instance.collection('users').doc(uid).delete();
-    print('store 삭제뒤');
+          //auth에서 삭제
+          await FirebaseAuth.instance.currentUser!.delete();
+          print('auth 삭제뒤');
+          //유저 파이어스토어 삭제
+          await FirebaseFirestore.instance.collection('users').doc(uid).delete();
+          print('store 삭제뒤');
 
-    //짝꿍의 user data가 파이어스토어에 없는지 확인 후 group 삭제 진행
-    final snapshot1 = await FirebaseFirestore.instance
-        .collection('groups')
-        .doc(groupId)
-        .get();
-    Map<String, dynamic> data = snapshot1.data as Map<String, dynamic>;
-    String? femaleUid = data['femaleUid'];
-    String? maleUid = data['maleUid'];
-    String? bukkungUid = myGender=='male'? femaleUid : maleUid;
-    final snapshot2 = await  FirebaseFirestore.instance.collection('users').doc(bukkungUid).get();
-    if (!snapshot2.exists){
-      //짝꿍의 user data가 파이어스토어에 없을 떄 (짝꿍이 이미 탈퇴를했을 때) 그룹 삭제진행
-     await FirebaseFirestore.instance.collection('groups').doc(groupId).delete();
-    }
-    _uploadFeedback();
-      //로그아웃
-      await UserRepository.signOut();
-    openAlertDialog(
-      title: '계정 탈퇴가 완료되었습니다.',
-      mainfunction: () {
-        SystemNavigator.pop();
-      },
-    );
+          //짝꿍의 user data가 파이어스토어에 없는지 확인 후 group 삭제 진행
+          final snapshot1 = await FirebaseFirestore.instance
+              .collection('groups')
+              .doc(groupId)
+              .get();
+          Map<String, dynamic> data = snapshot1.data as Map<String, dynamic>;
+          String? femaleUid = data['femaleUid'];
+          String? maleUid = data['maleUid'];
+          String? bukkungUid = myGender=='male'? femaleUid : maleUid;
+          final snapshot2 = await  FirebaseFirestore.instance.collection('users').doc(bukkungUid).get();
+          if (!snapshot2.exists){
+            //짝꿍의 user data가 파이어스토어에 없을 떄 (짝꿍이 이미 탈퇴를했을 때) 그룹 삭제진행
+            await FirebaseFirestore.instance.collection('groups').doc(groupId).delete();
+          }
+          _uploadFeedback();
+          //로그아웃
+          await UserRepository.signOut();
+          openAlertDialog(
+            title: '계정 탈퇴가 완료되었습니다.',
+            mainfunction: () {
+              SystemNavigator.pop();
+            },
+          );
+        });
   }
 
   void _uploadFeedback() {
