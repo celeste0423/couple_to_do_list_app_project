d330df473d51ee24a3f7517eeb499ec339bfa392
DentKim <ddosy99@gmail.com>
2023-08-05 02:58:18 +0900

    회원탈퇴 코드 수정

--- lib/features/setting/controller/auth_delete_page_controller.dart
+++ lib/features/setting/controller/auth_delete_page_controller.dart
@@ -15,13 +15,13 @@ class AuthDeletePageController extends GetxController {
   Rx<bool> isFeedback = false.obs;
 
   void authDeletion() async {
-    _uploadFeedback();
+
     //uid 임시저장
     String? uid = AuthController.to.user.value.uid;
     String? groupId = AuthController.to.user.value.groupId;
     String? myGender = AuthController.to.user.value.gender;
     //auth 삭제 전 재인증
-    openAlertDialog(
+    await openAlertDialog(
         title: '삭제',
         content: '원활한 삭제를 위해 계정 인증 부탁드립니다.',
         mainfunction: () async {
@@ -65,8 +65,10 @@ class AuthDeletePageController extends GetxController {
         });
     //auth에서 삭제
     await FirebaseAuth.instance.currentUser!.delete();
+    print('auth 삭제뒤');
     //유저 파이어스토어 삭제
     await FirebaseFirestore.instance.collection('users').doc(uid).delete();
+    print('store 삭제뒤');
 
     //짝꿍의 user data가 파이어스토어에 없는지 확인 후 group 삭제 진행
     final snapshot1 = await FirebaseFirestore.instance
@@ -80,8 +82,9 @@ class AuthDeletePageController extends GetxController {
     final snapshot2 = await  FirebaseFirestore.instance.collection('users').doc(bukkungUid).get();
     if (!snapshot2.exists){
       //짝꿍의 user data가 파이어스토어에 없을 떄 (짝꿍이 이미 탈퇴를했을 때) 그룹 삭제진행
-      FirebaseFirestore.instance.collection('groups').doc(groupId).delete();
+     await FirebaseFirestore.instance.collection('groups').doc(groupId).delete();
     }
+    _uploadFeedback();
       //로그아웃
       await UserRepository.signOut();
     openAlertDialog(
