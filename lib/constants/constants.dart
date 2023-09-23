7360ea295c31f5e141abc17d475527749ebea596
DentKim <ddosy99@gmail.com>
2023-09-24 00:36:44 +0900

    일단 기본이미지 나오도록 코드는 넣어놨음
    기존 url잘못 들어간 것은 따로 바꿔주거나 해야할 듯.

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,6 @@
 class Constants {
   //기본 꼬물이 이미지
+
   static String baseImageUrl =
       "https://firebasestorage.googleapis.com/v0/b/bukkunglist.appspot.com/o/bukkung_list%2F67b9ade0-ee36-11ed-b243-2f79762c93de%2FClosure%3A%20(%7BMap%3CString%2C%20dynamic%3E%3F%20options%7D)%20%3D%3E%20String%20from%20Function%20'v4'%3A..jpg?alt=media&token=f8db7acb-8888-4ca6-97f9-0a6ab237055d";
 // static String baseImageUrl =
