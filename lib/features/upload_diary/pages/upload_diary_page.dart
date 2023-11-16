f10c497254513949c3def0f63c06cde3c2bee4c2
celeste0423 <jyeob25@korea.ac.kr>
2023-11-16 16:28:45 +0900

    1.1.11 + 23 카테고리 다이얼로그 오버플로우 해결

--- lib/features/upload_diary/pages/upload_diary_page.dart
+++ lib/features/upload_diary/pages/upload_diary_page.dart
@@ -425,61 +425,89 @@ class UploadDiaryPage extends GetView<UploadDiaryController> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(25),
       ),
-      child: SizedBox(
-        height: 550,
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '카테고리',
-                  style: TextStyle(fontSize: 25),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      child: Get.width < 350 || Get.height < 550
+          ? SizedBox(
+              //화면이 너무 작을 때만 작동하는 예외처리 UI
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 20.0,
+                  mainAxisSpacing: 20.0,
+                  childAspectRatio: 2.0,
+                  shrinkWrap: true,
                   children: [
-                    SizedBox(
-                      height: 450,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          _categoryCard(
-                              'airplane', '여행', CustomColors.travel, '1travel'),
-                          _categoryCard('running', '액티비티',
-                              CustomColors.activity, '3activity'),
-                          _categoryCard(
-                              'study', '자기계발', CustomColors.study, '5study'),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 20),
-                    SizedBox(
-                      height: 450,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          _categoryCard(
-                              'food', '식사', CustomColors.meal, '2meal'),
-                          _categoryCard('singer', '문화활동', CustomColors.culture,
-                              '4culture'),
-                          _categoryCard(
-                              'filter-file', '기타', CustomColors.etc, '6etc'),
-                        ],
-                      ),
-                    )
+                    _categoryCard(
+                        'airplane', '여행', CustomColors.travel, '1travel'),
+                    _categoryCard(
+                        'running', '액티비티', CustomColors.activity, '3activity'),
+                    _categoryCard(
+                        'study', '자기계발', CustomColors.study, '5study'),
+                    _categoryCard('food', '식사', CustomColors.meal, '2meal'),
+                    _categoryCard(
+                        'singer', '문화활동', CustomColors.culture, '4culture'),
+                    _categoryCard(
+                        'filter-file', '기타', CustomColors.etc, '6etc'),
                   ],
                 ),
-              ],
+              ),
+            )
+          : SizedBox(
+              height: 550,
+              width: 350,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '카테고리',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 450,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                _categoryCard('airplane', '여행',
+                                    CustomColors.travel, '1travel'),
+                                _categoryCard('running', '액티비티',
+                                    CustomColors.activity, '3activity'),
+                                _categoryCard('study', '자기계발',
+                                    CustomColors.study, '5study'),
+                              ],
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          SizedBox(
+                            height: 450,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                _categoryCard(
+                                    'food', '식사', CustomColors.meal, '2meal'),
+                                _categoryCard('singer', '문화활동',
+                                    CustomColors.culture, '4culture'),
+                                _categoryCard('filter-file', '기타',
+                                    CustomColors.etc, '6etc'),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 
@@ -490,25 +518,27 @@ class UploadDiaryPage extends GetView<UploadDiaryController> {
         Get.back();
       },
       child: Container(
-        width: Get.width * 0.3,
-        height: Get.width * 0.3,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(25),
           color: color,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/icons/$icon.png',
-              width: 60,
-            ),
-            SizedBox(height: 10),
-            Text(
-              text,
-              style: TextStyle(fontSize: 15, color: Colors.white),
-            ),
-          ],
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 30, right: 30, top: 20, bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icons/$icon.png',
+                width: 60,
+              ),
+              SizedBox(height: 10),
+              Text(
+                text,
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
