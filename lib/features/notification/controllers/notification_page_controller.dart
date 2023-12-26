5cbe3c74c5321a4f840c091515e4ad9521404f12
celeste0423 <jyeob25@gmail.com>
2023-12-26 11:27:40 +0900

    1.1.16 + 28 알림페이지 제작중

--- lib/features/notification/controllers/notification_page_controller.dart
+++ lib/features/notification/controllers/notification_page_controller.dart
@@ -1,5 +1,34 @@
+import 'package:couple_to_do_list_app/features/auth/controller/auth_controller.dart';
+import 'package:couple_to_do_list_app/models/notification_model.dart';
+import 'package:couple_to_do_list_app/repository/notification_repository.dart';
 import 'package:get/get.dart';
 
 class NotificationPageController extends GetxController {
   static NotificationPageController get to => Get.find();
+
+  late List<NotificationModel> notifications;
+  bool isLoading = true;
+
+  @override
+  void onInit() {
+    _getAllNotification();
+    super.onInit();
+  }
+
+  void _getAllNotification() async {
+    isLoading = true;
+    notifications = await NotificationRepository().getAllNotifications(
+      AuthController.to.user.value.uid!,
+    );
+    isLoading = false;
+    update(['notificationBuilder'], true);
+  }
+
+  void updateIsChecked(String notificationId) async {
+    await NotificationRepository().updateIsChecked(notificationId);
+  }
+
+  void openPage(NotificationModel notification) {
+    //페이지 이동 만들기, 타입하고 contentId에 따라, 현재 페이지는 종료하고
+  }
 }
--- lib/repository/notification_repository.dart
+++ lib/repository/notification_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:couple_to_do_list_app/features/auth/controller/auth_controller.dart';
 import 'package:couple_to_do_list_app/models/notification_model.dart';
 
 class NotificationRepository {
@@ -27,6 +28,46 @@ class NotificationRepository {
     } catch (e) {
       print("notification 문서 삭제 오류: $e");
       rethrow;
+    }
+  }
+
+  Future<List<NotificationModel>> getAllNotifications(String uid) async {
+    try {
+      // 해당 사용자의 notification 서브컬렉션에 대한 쿼리
+      print('알림 받아오기 시작');
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(uid)
+              .collection('notification')
+              .get();
+      List<NotificationModel> notifications = querySnapshot.docs
+          .map((QueryDocumentSnapshot<Map<String, dynamic>> doc) =>
+              NotificationModel.fromJson(doc.data()!))
+          .toList();
+      print('알림 받아왔음 ${notifications.length}');
+      return notifications;
+    } catch (e) {
+      // 오류 처리
+      print('getAllNotifications 오류: $e');
+      return []; // 오류가 발생하면 빈 리스트를 반환
+    }
+  }
+
+  Future<void> updateIsChecked(String notificationId) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(AuthController.to.user.value.uid)
+          .collection('notification')
+          .doc(notificationId)
+          .update({
+        'is_checked': true,
+      });
+    } catch (e) {
+      // 오류 처리
+      print('updateIsChecked 오류: $e');
+      // throw e; // 오류를 다시 던지거나 처리 방법에 따라 적절히 처리
     }
   }
 
