4ebb548851d0665ae42c3302af85b326449f16ab
celeste0423 <jyeob25@gmail.com>
2024-04-15 11:56:45 +0900

    1.3.1+46 카톡 채널주소 변경

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -3,8 +3,9 @@ class Constants {
 
   static String baseImageUrl =
       "https://firebasestorage.googleapis.com/v0/b/bukkunglist.appspot.com/o/custom_app_image%2Fbukkung_list_icon.png?alt=media&token=977a5b74-602e-458b-b009-3554ac568acc";
-  static String baseImageUrl2 = "https://firebasestorage.googleapis.com/v0/b/bukkunglist.appspot.com/o/bukkung_list%2F67b9ade0-ee36-11ed-b243-2f79762c93de%2FClosure%3A%20(%7BMap%3CString%2C%20dynamic%3E%3F%20options%7D)%20%3D%3E%20String%20from%20Function%20'v4'%3A..jpg?alt=media&token=f8db7acb-8888-4ca6-97f9-0a6ab237055d";
-static String baseImageUrl3 =
+  static String baseImageUrl2 =
+      "https://firebasestorage.googleapis.com/v0/b/bukkunglist.appspot.com/o/bukkung_list%2F67b9ade0-ee36-11ed-b243-2f79762c93de%2FClosure%3A%20(%7BMap%3CString%2C%20dynamic%3E%3F%20options%7D)%20%3D%3E%20String%20from%20Function%20'v4'%3A..jpg?alt=media&token=f8db7acb-8888-4ca6-97f9-0a6ab237055d";
+  static String baseImageUrl3 =
       "https://firebasestorage.googleapis.com/v0/b/bukkunglist.appspot.com/o/bukkung_list%2F67b9ade0-ee36-11ed-b243-2f79762c93de%2FClosure%3A%20(%7BMap%3CString%2C%20dynamic%3E%3F%20options%7D)%20%3D%3E%20String%20from%20Function%20'v4'%3A..jpg?alt=media&token=f8db7acb-8888-4ca6-97f9-0a6ab237055d";
 
   //공지사항 노션 페이지
@@ -16,10 +17,10 @@ static String baseImageUrl3 =
       "https://www.notion.so/d0807c83075e438abcc9d8168861a6e5?pvs=4";
 
   //카톡 채널 페이지
-  static String kakaoChannel = "https://pf.kakao.com/_pTKhG";
+  static String kakaoChannel = "http://pf.kakao.com/_uElNG";
 
   //카톡 1대1 문의사항
-  static String kakaoQuestionChat = "https://pf.kakao.com/_pTKhG/chat";
+  static String kakaoQuestionChat = "http://pf.kakao.com/_uElNG/chat";
 
   //플레이 스토어 링크
   static String playstoreurl =
